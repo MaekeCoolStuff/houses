1ca9777ccbb221e6e9a6ed12a281f1aaead033bf
Mae van Dijk <mae.vandijk1990@gmail.com>
2021-03-09 21:21:34 +0100

    using dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 const app = express();
-const PORT = 8000;
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(
+    `[server]: Server is running at http://localhost:${process.env.PORT}`
+  );
 });
